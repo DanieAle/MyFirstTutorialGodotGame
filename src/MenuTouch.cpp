eef52c6b0f6585c6365a95f7e657c0be9189b605
Ibrahim Daniel Alejandro <danaleibrahim@gmail.com>
2026-02-27 23:37:46 -0300

    change asset for button and add blabel

--- src/MenuTouch.cpp
+++ src/MenuTouch.cpp
@@ -7,7 +7,6 @@ void GDMenuTouch::_register_methods(){
     register_method("_init", &GDMenuTouch::_init);
     register_method("_button_down", &GDMenuTouch::_button_down);
     register_method("_time_end", &GDMenuTouch::_time_end);
-    register_method("_game_Over", &GDMenuTouch::_game_Over);
     register_method("_restart", &GDMenuTouch::_restart);
     
     register_signal<GDMenuTouch, bool>((char *)"_start", "play",GODOT_VARIANT_TYPE_BOOL);
@@ -24,15 +23,13 @@ GDMenuTouch::~GDMenuTouch(){
 void GDMenuTouch::_ready(){
     Godot::print("Menu Ready...");
     button = Object::cast_to<TouchScreenButton>(get_node("Button"));
-    button->connect("pressed",this,"_button_down");
+    button->connect("released",this,"_button_down");
     myLabel = Object::cast_to<Label>(get_node("Titulo"));
     myTimer = Object::cast_to<Timer>(get_node("Timer"));
     myTimer->connect("timeout", this, "_time_end");
 }
 void GDMenuTouch::_init(){
     Godot::print("Menu Init...");
-    tiempo_transcurrido = 0.0;
-    start = false;
 }
 
 void GDMenuTouch::_button_down(){
@@ -47,17 +44,7 @@ void GDMenuTouch::_time_end(){
     Godot::print("Timer end");
     emit_signal("_start", true);
 }
-void GDMenuTouch::_game_Over(){
-    myLabel2->set_text("0");
-    start = false;
-    tiempo_transcurrido = 0.0;
-    myLabel->set_text(myTitles[2]);
-    myLabel->set_visible(true);
-    myTimer2->start();
-}
 void GDMenuTouch::_restart(){
     myLabel->set_text(myTitles[0]);
     button->set_visible(true);
-    tiempo_transcurrido = 0.0;
-    start = false;
 }
